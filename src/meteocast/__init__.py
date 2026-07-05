952d3485a4fc807d2666698758cc9d038e9b167a
Gilles Faucherand <gilles.faucherand@gmail.com>
2026-07-05 20:00:37 +0200

    0.2.0

--- src/meteocast/__init__.py
+++ src/meteocast/__init__.py
@@ -11,7 +11,7 @@ from meteocast.client import (
 )
 from meteocast.errors import LocationNotFoundError, MeteocastError, ProviderError
 
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 
 __all__ = [
     "Weather",
